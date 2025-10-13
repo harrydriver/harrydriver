3713d82b46c236b65dd8bf0c2b5279c3043cd034
Harry Driver <harrydriver2024@gmail.com>
2025-10-13 09:24:56 +0100

    commit

---
+++ hello_name_age.py
@@ -0,0 +1,5 @@
+name = input("What is your name?")
+year = int(input("What is the current year? "))
+birthyear = int(input("What year were you born? "))
+age = year - birthyear
+print(f"Hello, {name}! You are {age} years old this year.")
